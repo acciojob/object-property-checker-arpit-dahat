22b86ddbd9316c592064d183a7d81288bd64468b
arpit-dahat <118678798+arpit-dahat@users.noreply.github.com>
2023-07-09 22:28:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	key in sampleObject;
+	 return key in sampleObject;
 	}
 }
 
